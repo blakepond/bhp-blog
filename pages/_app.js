f776be09a2c89a7e3fa52a01854307c68d76d0c7
Blake Pond <blakehp@icloud.com>
2021-12-28 19:00:18 -0700

    stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Layout } from "../components";
 import "tailwindcss/tailwind.css";
-import "../styles/globals.scss";
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import "../../styles/globals.scss";
+
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
